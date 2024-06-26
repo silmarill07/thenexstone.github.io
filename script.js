1816a91454cca63ccc4c2838bb090a90313c0be0
silmarill07 <56761613+silmarill07@users.noreply.github.com>
2024-06-26 21:22:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  let savedTitle = localStorage.getItem('title');
+  let savedContent = localStorage.getItem('content');
+
+  if (savedTitle) {
+    document.getElementById('main-title').textContent = savedTitle;
+  }
+  if (savedContent) {
+    document.getElementById('main-content').textContent = savedContent;
+  }
+}
